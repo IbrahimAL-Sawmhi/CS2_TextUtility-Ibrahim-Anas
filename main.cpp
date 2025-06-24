5823f4d982c225b64dd62c705a2acb5d7c5f5320
Anas <a.h.a.b.b4@gmail.com>
2025-06-24 19:00:35 +0300

    Deleted text_functions.cpp text_functions.h

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@ int main()
 
     char arr[] = " ibrahim Ibrahim";
     cout << char_Count(arr) << endl; // 1:
-    cout << word_count(arr) << endl; //2:
-    
+    cout << word_count(arr) << endl; //2
 
 
 }
